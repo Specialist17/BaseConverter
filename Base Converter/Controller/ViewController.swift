4f086963b08521d5d876d15b4ca90f3e785555c7
Specia1ist17 <arocho.fernando.13@gmail.com>
2018-03-25 21:18:36 -0400

    added picker view with options

--- Base Converter/Controller/ViewController.swift
+++ Base Converter/Controller/ViewController.swift
@@ -11,14 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var valueEntryTextfield: UITextField!
-    @IBOutlet weak var binaryBtn: UIButton!
-    @IBOutlet weak var decimalBtn: UIButton!
+    @IBOutlet weak var basePicker: UIPickerView!
     
     let converter = BaseConverter()
     
     let placeholder = NSAttributedString(string: "Enter a value...", attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.5147656488, green: 0.5147656488, blue: 0.5147656488, alpha: 1), NSAttributedStringKey.font: UIFont(name: "Menlo", size: 36.0)!])
     override func viewDidLoad() {
         super.viewDidLoad()
+        basePicker.dataSource = self
+        basePicker.delegate = self
         
         valueEntryTextfield.attributedPlaceholder = placeholder
         
@@ -28,27 +29,16 @@ class ViewController: UIViewController {
     }
     
     @objc func textFieldTextDidChange() {
-        if valueEntryTextfield.text == "" {
-            disableButtons()
-        } else {
-            binaryBtn.isEnabled = true
-            binaryBtn.alpha = 1
-            decimalBtn.isEnabled = true
-            decimalBtn.alpha = 1
-        }
+       
     }
     
     func disableButtons() {
-        binaryBtn.isEnabled = false
-        binaryBtn.alpha = 0.5
-        decimalBtn.isEnabled = false
-        decimalBtn.alpha = 0.5
+       
     }
 
-    @IBAction func binaryBtnWasPressed(_ sender: UIButton) {
+    @IBAction func convertBtnWasPressed(_ sender: UIButton) {
         if valueEntryTextfield.text != "" {
-            binaryBtn.alpha = 1.0
-            decimalBtn.alpha = 0.5
+          
             guard let string = valueEntryTextfield.text else {return}
             let strings = string.split(separator: " ")
             var all_nums = [String]()
@@ -64,24 +54,20 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func decimalBtnWasPressed(_ sender: UIButton) {
-        if valueEntryTextfield.text != "" {
-            binaryBtn.alpha = 0.5
-            decimalBtn.alpha = 1.0
-            guard let string = valueEntryTextfield.text else {return}
-            let strings = string.split(separator: " ")
-            var all_nums = [String]()
-            for numbers in strings {
-                
-                let bitsFromString = numbers.map { Int(String($0))! }
-                let binaryDigit = BinaryDecimal(bitsFromString)
-                all_nums.append(binaryDigit.calculateIntValueForBinary())
-            }
-            
-            let new_str = all_nums.joined(separator: " ")
-            valueEntryTextfield.text = "\(new_str)"
-        }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
     }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CONVERSION_TYPES.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CONVERSION_TYPES[row]
+    }
 }
 
--- Base Converter/Helpers/Constants.swift
+++ Base Converter/Helpers/Constants.swift
@@ -19,3 +19,5 @@ let DECODING_CHARACTERS: [Character: Int] = ["0": 0, "1": 1, "2": 2, "3": 3, "4"
     "f": 15 , "g": 16 , "h": 17 , "i": 18 , "j": 19 , "k": 20 , "l": 21 ,
     "m": 22 , "n": 23 , "o": 24 , "p": 25 , "q": 26 , "r": 27 , "s": 28 ,
     "t": 29 , "u": 30 , "v": 31 , "w": 32 , "x": 33 , "y": 34 , "z": 35 ]
+
+let CONVERSION_TYPES = ["BINARY", "DECIMAL", "HEXADECIMAL", "OCTAL", "ASCII"]
